b61ca49dfdc2e17e5d1761f8ffcdc176946da6ea
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-04-17 18:08:04 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+# std
+from distutils.core import setup
+# noinspection PyUnresolvedReferences
+import setuptools  # see below (1)
+from pathlib import Path
+
+# (1) see https://stackoverflow.com/questions/8295644/
+# Without this import, install_requires won't work.
+
+
+keywords = [
+    "anki",
+    "pandas",
+]
+
+description = "Load your anki database as a pandas DataFrame with just one " \
+              "line of code!"
+
+this_dir = Path(__file__).resolve().parent
+
+packages = setuptools.find_packages()
+
+# todo: add readme as long_description
+# with (this_dir / "README.rst").open() as fh:
+#     long_description = fh.read()
+
+# todo: add versioning
+# with (this_dir / "clusterking" / "version.txt").open() as vf:
+#     version = vf.read()
+
+setup(
+    name='ankipandas',
+    # version=version,
+    packages=packages,
+    # install_requires=install_requires,
+    url="https://github.com/klieret/ankipandas",
+    project_urls={
+        "Bug Tracker": "https://github.com/klieret/ankipandas/issues",
+        # todo: add docs
+        "Documentation": "https://ankipandas.readthedocs.io/",
+        "Source Code": "https://github.com/klieret/ankipandas/",
+    },
+    package_data={
+        'clusterking': ['git_info.json', 'version.txt'],
+    },
+    license="MIT",
+    keywords=keywords,
+    description=description,
+    # long_description=long_description,
+    # long_description_content_type="text/x-rst",
+    # todo: add classifiers
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "License :: OSI Approved :: MIT License",
+    #     "Operating System :: OS Independent",
+    #     "Topic :: Scientific/Engineering :: Physics",
+    #     "Topic :: Scientific/Engineering :: Visualization"
+    # ],
+)
