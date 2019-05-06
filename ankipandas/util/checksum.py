41f43061bad57de2105d867110cf0c439d24a48f
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-05-06 12:00:33 +0200

    [Cleanup] Mark most content private

--- ankipandas/util/checksum.py
+++ ankipandas/util/checksum.py
@@ -5,32 +5,32 @@ from hashlib import sha1
 from html.entities import name2codepoint
 
 """ Directly copied from Anki: anki/anki/utils.py
-Only a bit of PEP8ing"""
+Only a bit of PEP8ing and making things private."""
 
-reComment = re.compile("(?s)<!--.*?-->")
-reStyle = re.compile("(?si)<style.*?>.*?</style>")
-reScript = re.compile("(?si)<script.*?>.*?</script>")
-reTag = re.compile("(?s)<.*?>")
-reEnts = re.compile(r"&#?\w+;")
-reMedia = re.compile("(?i)<img[^>]+src=[\"']?([^\"'>]+)[\"']?[^>]*>")
+_reComment = re.compile("(?s)<!--.*?-->")
+_reStyle = re.compile("(?si)<style.*?>.*?</style>")
+_reScript = re.compile("(?si)<script.*?>.*?</script>")
+_reTag = re.compile("(?s)<.*?>")
+_reEnts = re.compile(r"&#?\w+;")
+_reMedia = re.compile("(?i)<img[^>]+src=[\"']?([^\"'>]+)[\"']?[^>]*>")
 
 
-def strip_html(s):
-    s = reComment.sub("", s)
-    s = reStyle.sub("", s)
-    s = reScript.sub("", s)
-    s = reTag.sub("", s)
-    s = ents_to_txt(s)
+def _strip_html(s):
+    s = _reComment.sub("", s)
+    s = _reStyle.sub("", s)
+    s = _reScript.sub("", s)
+    s = _reTag.sub("", s)
+    s = _ents_to_txt(s)
     return s
 
 
-def strip_html_media(s):
+def _strip_html_media(s):
     "Strip HTML but keep media filenames"
-    s = reMedia.sub(" \\1 ", s)
-    return strip_html(s)
+    s = _reMedia.sub(" \\1 ", s)
+    return _strip_html(s)
 
 
-def ents_to_txt(html):
+def _ents_to_txt(html):
     # entitydefs defines nbsp as \xa0 instead of a standard space, so we
     # replace it first
     html = html.replace("&nbsp;", " ")
@@ -53,16 +53,24 @@ def ents_to_txt(html):
             except KeyError:
                 pass
         return text # leave as is
-    return reEnts.sub(fixup, html)
+    return _reEnts.sub(fixup, html)
 
 
-def checksum(data):
+def _checksum(data):
     if isinstance(data, str):
         data = data.encode("utf-8")
     return sha1(data).hexdigest()
 
 
-# todo: add test
-def field_checksum(data):
-    # 32 bit unsigned number from first 8 digits of sha1 hash
-    return int(checksum(strip_html_media(data).encode("utf-8"))[:8], 16)
+def field_checksum(data: str) -> int:
+    """ 32 bit unsigned number from first 8 digits of sha1 hash.
+    Apply this to the first field to the the field checksum that is used by
+    Anki to detect duplicates.
+
+    Args:
+        data: string like
+
+    Returns:
+        int
+    """
+    return int(_checksum(_strip_html_media(data).encode("utf-8"))[:8], 16)
