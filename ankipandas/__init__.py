e9fc7a0a2f3e009c912db6c0529ed8e4a068e710
Kilian <kilian.lieret@posteo.de>
2020-12-05 18:02:52 +0100

    [Feat] Allow to import set_debug_log_level from top level

--- ankipandas/__init__.py
+++ ankipandas/__init__.py
@@ -8,4 +8,4 @@ import ankipandas.collection
 from ankipandas.collection import Collection
 from ankipandas.paths import find_db, db_path_input
 from ankipandas.ankidf import AnkiDataFrame
-from ankipandas.util.log import log, set_log_level
+from ankipandas.util.log import log, set_log_level, set_debug_log_level
