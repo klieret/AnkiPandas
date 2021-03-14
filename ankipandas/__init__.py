63cf6f0d6b6e6562b327bee45b4618496e04755e
Kilian <kilian.lieret@posteo.de>
2021-03-14 11:27:46 +0100

    [Dev] Remove double imports

--- ankipandas/__init__.py
+++ ankipandas/__init__.py
@@ -1,9 +1,6 @@
 # ours
 import ankipandas.raw
-import ankipandas.paths
 import ankipandas.util
-import ankipandas.paths
-import ankipandas.collection
 from ankipandas.collection import Collection
 from ankipandas.paths import find_db, db_path_input
 from ankipandas.ankidf import AnkiDataFrame
